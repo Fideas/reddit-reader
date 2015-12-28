871caf9aa560c4af9cf99447a23cf24afb5b794d
Nicolas Carrasco-Stevenson <niccarra@gmail.com>
2015-12-28 12:02:22 -0300

    Add a test Ad to the main activity

--- app/src/main/java/com/nicolascarrasco/www/redditreader/MainActivity.java
+++ app/src/main/java/com/nicolascarrasco/www/redditreader/MainActivity.java
@@ -3,11 +3,26 @@ package com.nicolascarrasco.www.redditreader;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.ads.AdRequest;
+import com.google.android.gms.ads.AdView;
+
+import butterknife.Bind;
+import butterknife.ButterKnife;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Bind(R.id.adView) AdView mAdView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        ButterKnife.bind(this);
+        //Add your own device Id if testing on a real device
+        AdRequest adRequest = new AdRequest.Builder()
+                .addTestDevice(AdRequest.DEVICE_ID_EMULATOR)
+                .addTestDevice("F178C54519B81461A49075E590A1E90C")
+                .build();
+        mAdView.loadAd(adRequest);
     }
 }
