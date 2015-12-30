42669894ce79f6aee60c4113a011000a9c9e348e
Nicolas Carrasco-Stevenson <niccarra@gmail.com>
2015-12-30 18:25:23 -0300

    Create a simple class to represent a reddit post

---
+++ app/src/main/java/com/nicolascarrasco/www/redditreader/data/Post.java
@@ -0,0 +1,20 @@
+package com.nicolascarrasco.www.redditreader.data;
+
+/**
+ * Created by Nicolás Carrasco on 30/12/2015.
+ */
+public class Post {
+
+    private String mTitle;
+
+    public Post() {
+    }
+
+    public String getTitle() {
+        return mTitle;
+    }
+
+    public void setTitle(String title) {
+        mTitle = title;
+    }
+}
