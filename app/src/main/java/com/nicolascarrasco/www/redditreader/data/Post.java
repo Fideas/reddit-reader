95e8aabdf41b2fd1efe1db13da76a04c69ebda79
Nicolas Carrasco-Stevenson <niccarra@gmail.com>
2016-01-03 23:48:18 -0300

    Add parcelable support to the Post Class

--- app/src/main/java/com/nicolascarrasco/www/redditreader/data/Post.java
+++ app/src/main/java/com/nicolascarrasco/www/redditreader/data/Post.java
@@ -1,13 +1,41 @@
 package com.nicolascarrasco.www.redditreader.data;
 
+import android.os.Parcel;
+import android.os.Parcelable;
+
 /**
  * Created by Nicolás Carrasco on 30/12/2015.
  */
-public class Post {
+public class Post implements Parcelable {
+    public static final Parcelable.Creator<Post> CREATOR = new Parcelable.Creator<Post>() {
+        @Override
+        public Post createFromParcel(Parcel source) {
+            return new Post(source);
+        }
+
+        @Override
+        public Post[] newArray(int size) {
+            return new Post[size];
+        }
+    };
 
     private String mTitle;
 
     public Post() {
+    }
+
+    public Post(Parcel in) {
+        mTitle = in.readString();
+    }
+
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeString(mTitle);
     }
 
     public String getTitle() {
